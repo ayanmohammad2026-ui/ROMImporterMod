295501e4f31541f6dc3073ebdb0b487643ba2008
ayanmohammad2026-ui <ayanmohammad2026@gmail.com>
2026-07-27 15:40:38 +0500

    Implement GitHub link in MyModPopup
    Added a link button to GitHub in MyModPopup class.

--- main.cpp
+++ main.cpp
@@ -18,7 +18,39 @@ protected:
         );
         importButton->setPosition({0, 0});
 
-        this->m_buttonMenu->addChild(importButton);
+        this->m_buttonMenu->addChild(importButton);#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp> // Make sure this is at the very top of your file
+
+using namespace geode::prelude;
+
+class MyModPopup : public Popup {
+public:
+    bool init() {
+        if (!Popup::init()) return false;
+
+        // ... (all your other UI setup code for your mod goes here) ...
+
+        // Paste the link code right down here at the end of the init function:
+        auto linkLabel = CCLabelBMFont::create("GitHub Link", "goldFont.fnt");
+        linkLabel->setScale(0.5f);
+
+        auto linkButton = CCMenuItemSpriteExtra::create(
+            linkLabel,
+            this,
+            [](CCObject*) {
+                geode::utils::web::openLink("https://github.com/ayanmohammad2026-ui/ROMImporterMod");
+            }
+        );
+
+        auto menu = CCMenu::create();
+        menu->addChild(linkButton);
+        menu->setPosition({m_size.width / 2, 20.f});
+        this->addChild(menu);
+
+        return true; // Keep this as the very last line of the init function
+    }
+};
+
         return true;
     }
 
